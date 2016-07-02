a218807f3fa1c3b17399de2bf23f7cbb8d7c975f
Weber Amaral <weberamaral@weberamaral.home>
2016-07-02 17:01:27 -0300

    Update app sample

--- app/angular/app.js
+++ app/angular/app.js
@@ -1,3 +1,3 @@
 (function() { 'use strict';
-  
+  angular.module('webcam', ['ng-webcam']);
 })();
--- app/angular/controller.js
+++ app/angular/controller.js
@@ -1,3 +1,32 @@
 (function() { 'use strict';
+  angular.module('webcam').controller('webcamController', webcamController);
+  webcamController.$inject = ['$scope', '$log'];
+  function webcamController($scope, $log) {
+    /* jshint validthis: true */
+    var vm = this;
+    vm.config = {
 
+    };
+    vm.onComplete = function(src) {
+      $log.log('onComplete : ', src);
+    };
+    vm.onError = function(err) {
+      $log.error('onError : ', err);
+    };
+    vm.onLoad = function() {
+      $log.info('onLoad');
+    };
+    vm.onCapturing = function(src) {
+      $log.info('onCapturing : ', src);
+    };
+    vm.capture = function() {
+      $scope.$broadcast('ngWebcam_capture');
+    };
+    vm.on = function() {
+      $scope.$broadcast('ngWebcam_on');
+    };
+    vm.off = function() {
+      $scope.$broadcast('ngWebcam_off');
+    };
+  }
 })();
