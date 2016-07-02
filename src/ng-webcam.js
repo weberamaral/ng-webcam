79314d301ab736fde0e8a553641e11772f403db1
Weber Amaral <weberamaral@weberamaral.home>
2016-07-02 12:14:47 -0300

    Initial config project

---
+++ src/ng-webcam.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+  /**
+   *
+   */
+  angular.module('ng-webcam').directive('ngWebcam', ngWebcam);
+  ngWebcam.$inject = [];
+  /**
+   *
+   */
+  function ngWebcam() {}
+})();
