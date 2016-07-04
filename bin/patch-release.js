972ed67b918bd3cd6ed1160f1b6bd04ae396aa01
Weber Amaral <weberamaral@weberamaral.home>
2016-07-03 22:14:12 -0300

    Update release

--- bin/patch-release.js
+++ bin/patch-release.js
@@ -4,4 +4,7 @@ console.log('patch-release');
 var shell = require('shelljs');
 
 shell.exec('gulp clean && gulp build');
+shell.exec('npm version patch');
 shell.exec('git add -A . && git commit -a -m \'Update release\'');
+shell.exec('git push origin master --force');
+shell.exec('git push origin --tags');
