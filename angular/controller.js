7dd727360a76febe1ee195b9a0950af71277992b
Weber Amaral <weberamaral@weberamaral.home>
2016-07-05 02:04:29 -0300

    Update sample project

--- angular/controller.js
+++ angular/controller.js
@@ -9,7 +9,7 @@
       shots: 3,
       viewerWidth: 480,
       viewerHeight: 320,
-      flashFallbackUrl: 'bower_components/webcamjs/webcam.swf',
+      flashFallbackUrl: 'vendors/webcamjs/webcam.swf',
       shutterUrl: 'shutter.mp3',
       flashNotDetectedText: 'Seu browser não atende os requisitos mínimos para utilização da camera. ' +
       'Instale o ADOBE Flash player ou utilize os browsers (Google Chrome, Firefox ou Edge)'
@@ -17,9 +17,17 @@
 
     vm.showButtons = false;
     vm.captureButtonEnable = false;
+    vm.progress = 0;
 
     vm.onCaptureComplete = function(src) {
       $log.log('webcamController.onCaptureComplete : ', src);
+      vm.progress = 100;
+      var el = document.getElementById('result');
+      var img = document.createElement('img');
+      img.src = src[vm.config.shots-1];
+      img.width = 240;
+      img.height = 180;
+      el.appendChild(img);
     };
     vm.onError = function(err) {
       $log.error('webcamController.onError : ', err);
@@ -34,10 +42,17 @@
       vm.captureButtonEnable = true;
     };
     vm.onCaptureProgress = function(src, progress) {
+      vm.progress = progress;
       var result = {
         src: src,
         progress: progress
       }
+      var el = document.getElementById('result');
+      var img = document.createElement('img');
+      img.src = src;
+      img.width = 240;
+      img.height = 180;
+      el.appendChild(img);
       $log.info('webcamController.onCaptureProgress : ', result);
     };
     vm.capture = function() {
