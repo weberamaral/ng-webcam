9c9e5c6f2202314aeeb534727df078bf5d9d9b65
Weber Amaral <weberamaral@outlook.com>
2016-07-05 18:25:33 -0300

    Update app

--- app/angular/controller.js
+++ app/angular/controller.js
@@ -7,7 +7,6 @@
     vm.config = {
       delay: 2,
       shots: 3,
-      countdown: 3,
       flashFallbackUrl: 'vendors/webcamjs/webcam.swf',
       shutterUrl: 'shutter.mp3',
       flashNotDetectedText: 'Seu browser não atende os requisitos mínimos para utilização da camera. ' +
